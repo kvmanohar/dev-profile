7dbd8816930b57c5b71837f33e7587881f4f21be
Manohar Kurapati <kvmanohar@gmail.com>
2017-10-28 08:22:50 +0100

    Screen build for iphone.

---
+++ dev-profile/Views/CurvedUIImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CurvedUIImageView.swift
+//  dev-profile
+//
+//  Created by Manohar Kurapati on 26/10/2017.
+//  Copyright © 2017 Manosoft. All rights reserved.
+//
+
+import UIKit
+
+class CurvedUIImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
